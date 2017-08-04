3b4f74b1a35b017d3f4073d5e50e21e210f7b7ec
Administrator <Administrator@MS-20170724SSHJ>
2017-08-04 08:21:34 +0800

    11

---
+++ LeaRun.Application/LeaRun.Application.Busines/AuthorizeManage/Base_ProjectBLL.cs
@@ -0,0 +1,87 @@
+using LeaRun.Application.Entity.AuthorizeManage;
+using LeaRun.Application.IService.AuthorizeManage;
+using LeaRun.Application.Service.AuthorizeManage;
+using LeaRun.Util.WebControl;
+using System.Collections.Generic;
+using System;
+
+namespace LeaRun.Application.Busines.AuthorizeManage
+{
+    /// <summary>
+    /// 版 本 LearunADMS V6.1.2.0
+    /// Copyright (c) 2013-2016 上海力软信息技术有限公司
+    /// 创 建：超级管理员
+    /// 日 期：2017-08-02 14:34
+    /// 描 述：项目
+    /// </summary>
+    public class Base_ProjectBLL
+    {
+        private Base_ProjectIService service = new Base_ProjectService();
+
+        #region 获取数据
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="pagination">分页</param>
+        /// <param name="queryJson">查询参数</param>
+        /// <returns>返回分页列表</returns>
+        public IEnumerable<Base_ProjectEntity> GetPageList(Pagination pagination, string queryJson)
+        {
+            return service.GetPageList(pagination, queryJson);
+        }
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="queryJson">查询参数</param>
+        /// <returns>返回列表</returns>
+        public IEnumerable<Base_ProjectEntity> GetList(string queryJson)
+        {
+            return service.GetList(queryJson);
+        }
+        /// <summary>
+        /// 获取实体
+        /// </summary>
+        /// <param name="keyValue">主键值</param>
+        /// <returns></returns>
+        public Base_ProjectEntity GetEntity(string keyValue)
+        {
+            return service.GetEntity(keyValue);
+        }
+        #endregion
+
+        #region 提交数据
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        public void DeleteEntity(string keyValue)
+        {
+            try
+            {
+                service.DeleteEntity(keyValue);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        /// <summary>
+        /// 保存表单（新增、修改）
+        /// </summary>
+        /// <param name="keyValue">主键值</param>
+        /// <param name="entity">实体对象</param>
+        /// <returns></returns>
+        public void SaveEntity(string keyValue, Base_ProjectEntity entity)
+        {
+            try
+            {
+                service.SaveEntity(keyValue, entity);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
